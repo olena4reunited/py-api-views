c414871ecc43ecf9fe0bfad9b3c72193101826ca
olena4reunited <olenaduringintermission@gmail.com>
2024-09-29 22:27:58 +0300

    git commit -m 'build: API views and set url-routers due to requirements'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre, Actor, CinemaHall
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["id", "name"]
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ["id", "first_name", "last_name"]
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ["id", "name", "rows", "seats_in_row"]
 
 
 class MovieSerializer(serializers.Serializer):
